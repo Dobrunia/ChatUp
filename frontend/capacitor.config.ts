bd4f4a63b93ac5ac9f02a1cab9a24b954ca36f10
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2026-02-25 23:32:27 +0300

    refactor

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.chatup.app',
-  appName: 'ChatUp',
-  webDir: 'dist'
-};
-
-export default config;
